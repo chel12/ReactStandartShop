5f773d413827b0b12efa7e1ebd37ed6e19f2de78
Semyon <sema.chel12@gmail.com>
2024-09-08 16:50:34 +0300

    components:Poster

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Poster from '../Poster/Poster';
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div>
+			<Poster />
+		</div>
+	);
 };
 
 export default Home;
