583857569ef763670099b36fdefcb44059917b9e
Semyon <sema.chel12@gmail.com>
2024-09-08 17:53:59 +0300

    redux:products

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Poster from '../Poster/Poster';
+import Products from '../Products/Products';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
+	const { list } = useSelector(({ products }) => products);
+
 	return (
 		<div>
 			<Poster />
+			<Products products={list} amount={5} title="Trending" />
 		</div>
 	);
 };
