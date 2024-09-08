eb476319f58def28f2abf3319dc2fc7aea67cec8
Semyon <sema.chel12@gmail.com>
2024-09-08 15:35:14 +0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import styles from '../../styles/Footer.module.css';
+import LOGO from '../../images/logo.svg';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../utils/routes';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<section className={styles.footer}>
+			<div className={styles.logo}>
+				<Link to={ROUTES.HOME}>
+					<img src={LOGO} alt="logo" />
+				</Link>
+			</div>
+			<div className={styles.rights}>Developed by Sima4</div>
+			<div className={styles.socials}>
+				<a href="instagram" target="_blank">
+					<svg className={'icon'}>
+						<use
+							xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#instagram`}
+						/>
+					</svg>
+				</a>
+				<a href="facebook" target="_blank">
+					<svg className={'icon'}>
+						<use
+							xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#facebook`}
+						/>
+					</svg>
+				</a>
+				<a href="youtube" target="_blank">
+					<svg className={'icon'}>
+						<use
+							xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`}
+						/>
+					</svg>
+				</a>
+			</div>
+		</section>
+	);
 };
 
 export default Footer;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import styles from '../../styles/Sidebar.module.css';
 const Sidebar = () => {
 	return <div>Sidebar</div>;
 };
