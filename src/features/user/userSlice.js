edd990d0d86f4eef0286b2ff417c4222b31caf63
Semyon <sema.chel12@gmail.com>
2024-09-14 15:40:40 +0300

    components:UserSignupForm + redux--logic

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,26 +1,28 @@
-import { createAsyncThunk, createSlice, current } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { BASE_URL } from '../../utils/constants';
 
-// export const getCategories = createAsyncThunk(
-// 	'categories/getCategories',
-// 	async (_, thunkAPI) => {
-// 		try {
-// 			const res = await axios(`${BASE_URL}/categories`);
-// 			return res.data;
-// 		} catch (err) {
-// 			console.log(err);
-// 			return thunkAPI.rejectWithValue(err);
-// 		}
-// 	}
-// );
+export const createUser = createAsyncThunk(
+	'users/createUser',
+	async (payload, thunkAPI) => {
+		try {
+			const res = await axios.post(`${BASE_URL}/users`, payload);
+			return res.data;
+		} catch (err) {
+			console.log(err);
+			return thunkAPI.rejectWithValue(err);
+		}
+	}
+);
 
 const userSlice = createSlice({
 	name: 'user',
 	initialState: {
-		currentUser: [],
+		currentUser: {},
 		cart: [],
 		isLoading: false,
+		formType: 'signup',
+		showForm: false,
 	},
 	reducers: {
 		addItemToCart: (state, { payload }) => {
@@ -29,26 +31,24 @@ const userSlice = createSlice({
 			if (found) {
 				newCart = newCart.map((item) => {
 					return item.id === payload.id
-						? 
-						{...item, quantity: payload.quantity || item.quantity + 1}
+						? {
+								...item,
+								quantity: payload.quantity || item.quantity + 1,
+						  }
 						: item;
 				});
 			} else newCart.push({ ...payload, quantity: 1 });
 			state.cart = newCart;
 		},
+		toggleForm:(state,{payload})=>{
+			state.showForm = payload
+		}
 	},
 	extraReducers: (builder) => {
-		// builder.addCase(getCategories.pending, (state) => {
-		// 	state.isLoading = true;
-		// });
-		// builder.addCase(getCategories.fulfilled, (state, { payload }) => {
-		// 	state.list = payload;
-		// 	state.isLoading = false;
-		// });
-		// builder.addCase(getCategories.rejected, (state) => {
-		// 	state.isLoading = false;
-		// });
+		builder.addCase(createUser.fulfilled, (state, { payload }) => {
+			state.currentUser = payload;
+		});
 	},
 });
-export const { addItemToCart } = userSlice.actions;
+export const { addItemToCart, toggleForm } = userSlice.actions;
 export default userSlice.reducer;
