d9fdf5f5b03341b2f29d6059deee430c2a02457e
Semyon <sema.chel12@gmail.com>
2024-09-14 10:54:11 +0300

    redux.reducers:getRelatedProducts+utils:common.shuffle(random for related products

--- src/components/Products/SingleProducts.jsx
+++ src/components/Products/SingleProducts.jsx
@@ -3,10 +3,15 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useGetProductQuery } from '../../features/api/apiSlice';
 import { ROUTES } from '../../utils/routes';
 import Product from './Product';
+import Products from './Products';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRelatedProducts } from '../../features/products/productsSlice';
 
 const SingleProducts = () => {
+	const dispatch = useDispatch();
 	const { id } = useParams();
 	const navigate = useNavigate();
+	const { list, related } = useSelector(({ products }) => products);
 	const { data, isLoading, isFetching, isSuccess } = useGetProductQuery({
 		id,
 	});
@@ -16,12 +21,19 @@ const SingleProducts = () => {
 			navigate(ROUTES.HOME);
 		}
 	}, [isLoading, isFetching, isSuccess]);
-	console.log(data);
+
+	useEffect(() => {
+		if (!data || !list.length) return;
+
+		dispatch(getRelatedProducts(data.category.id));
+	}, [data, dispatch, list.length]);
+
 	return !data ? (
 		<section className="preloader">Loading...</section>
 	) : (
 		<>
 			<Product {...data} />
+			<Products products={related} amount={5} title="Related products" />
 		</>
 	);
 };
